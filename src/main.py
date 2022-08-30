a3b61c4f920a23a9a5f01e8357a8065648b6d281
Ibrahim Zarraga <ibrahimzarragaam@gmail.com>
2022-08-30 17:46:53 -0400

    Done

--- src/main.py
+++ src/main.py
@@ -8,12 +8,14 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Todos
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+db_url = os.getenv('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
@@ -30,14 +32,52 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/todos', methods=['GET'])
+def handle_todos():
+    if request.method == 'GET':
+        todos = Todos.query.all()
+        return jsonify(list(map(lambda item: item.serialize(), todos))), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/todos', methods=['POST'])
+def add_new_user():
+    if request.method == 'POST':
+        body = request.json
 
-    return jsonify(response_body), 200
+        if body.get("label") is None:
+            return jsonify({"message": "Error, property bad"}), 400 
+        elif body.get("done") is None:
+            return jsonify({"message": "Error, property bad"}), 400
+
+        new_todo = Todos(label=body.get("label"), done=body.get("done"))
+        db.session.add(new_todo)
+
+        try:
+            db.session.commit()
+            return jsonify(new_todo.serialize()), 201
+        except Exception as error:
+            print(error.args)
+            db.session.rollback()
+            return jsonify({"message": f"Error {error.args}"}), 500
+
+@app.route('/todos/<int:todo_id>', methods=['DELETE'])
+def delete_user(todo_id = None):
+    if request.method == 'DELETE':
+        if todo_id is None:
+            return jsonify({"message": "Error, bad request"}), 400
+        elif todo_id is not None:
+            deleted_todo = Todos.query.get(todo_id)
+            if deleted_todo is None:
+                return jsonify({"message": "Error, couldn't find user"}), 404
+            else:
+                db.session.delete(deleted_todo)
+
+            try:
+                db.session.commit()
+                return jsonify([]), 204
+            except Exception as error:
+                print(error.args)
+                db.session.rollback()
+                return jsonify({"message": f"Error {error.args}"})
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
